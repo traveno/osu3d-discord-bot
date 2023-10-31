bb76e68c01f9513e091b582459407ed82e90e6ed
Stephen <teamgreensf@gmail.com>
2023-10-31 04:47:14 -0700

    Update for new db schema

--- src/josef.ts
+++ src/josef.ts
@@ -1,7 +1,7 @@
 import { Client, Events, GatewayIntentBits, EmbedBuilder, Guild, TextChannel, RoleResolvable } from 'discord.js';
 import { SupabaseWatcher } from './supabase';
 import { debugMode } from './index';
-
+import Timeout from 'smart-timeout';
 
 export enum PermCategory {
   TIER_1 = 0,
@@ -68,12 +68,12 @@ export class Josef {
   }
 
   private _registerEvents() {
-    this._supabaseWatcher.monitorTable('user_levels', payload => this._onUserLevelEvent(payload));
-    this._supabaseWatcher.monitorTable('machine_events', payload => this._announceMachineEvent(payload));
-    this._supabaseWatcher.monitorTable('profiles', payload => this._onProfileEvent(payload));
+    this._supabaseWatcher.monitorTable('user_levels', payload => this._onUserLevelCRUD(payload));
+    this._supabaseWatcher.monitorTable('machine_events', payload => this._onMachineEventCRUD(payload));
+    this._supabaseWatcher.monitorTable('profiles', payload => this._onProfilesCRUD(payload));
     this._supabaseWatcher.monitorChannel('discord-ping', payload => this._pingDiscordUser(payload));
-    this._supabaseWatcher.monitorTable('prints', payload => this._onPrintUpdated(payload));
-    this._supabaseWatcher.monitorTable('inv_changes', payload => this._onInventoryChange(payload));
+    this._supabaseWatcher.monitorTable('prints', payload => this._onPrintsCRUD(payload));
+    this._supabaseWatcher.monitorTable('inv_changes', payload => this._onInvChangeCRUD(payload));
   }
 
   private _createClient() {
@@ -82,7 +82,7 @@ export class Josef {
     this._discordClient.once(Events.ClientReady, client => this._onceClientReady(client));
   }
 
-  private async _onInventoryChange(payload: any) {
+  private async _onInvChangeCRUD(payload: any) {
     if (debugMode) console.log('_onInventoryChange()', payload);
     if (payload.eventType !== 'INSERT') return;
 
@@ -105,18 +105,22 @@ export class Josef {
     }
   }
 
-  private async _onPrintUpdated(payload:any) {
-    if (debugMode) console.log('_onPrintUpdated()');
-    if (payload.eventType !== 'UPDATE') return;
-    if (payload.new.canceled === payload.old.canceled) return;
-    
-    const discordName = (await this._supabaseWatcher.getDiscordName(payload.new.created_by_user_id)).data?.discord as string | null;
+  private async _scheduleUserNotification(print_id: string, user_id: string, message: string, timeout: number) {
+    const discordName = (await this._supabaseWatcher.getDiscordName(user_id)).data?.discord as string | null;
     if (!discordName) return;
 
-    const allMembers = await this._discordGuild!.members.fetch();
-    const user = allMembers.find(m => m.user.username === discordName);
+    Timeout.set(print_id, () => this._sendUserNotification(discordName, message), timeout);
+  }
 
-    user?.send('One of your prints was just canceled.');
+  private async _onPrintsCRUD(payload: any) {
+    if (debugMode) console.log('_onPrintsCRUD()', payload);
+
+    // Set up a notification for the user when print completes
+    if (payload.eventType === 'INSERT') {
+      const timeout = new Date(payload.new.done_at).getTime() - new Date().getTime();
+      this._scheduleUserNotification(payload.new.id, payload.new.created_by_user_id, '[OSU3D] One of your prints has completed!', timeout);
+      return;
+    }
   }
 
   private async _onceClientReady(client: Client) {
@@ -133,17 +137,17 @@ export class Josef {
   }
 
   private async _pingDiscordUser(payload: any) {
-    if (debugMode) console.log('_pingDiscordUser()');
+    if (debugMode) console.log('_pingDiscordUser()', payload);
     const discordName = payload.discord;
 
     const allMembers = await this._discordGuild!.members.fetch();
     const user = allMembers.find(m => m.user.username === discordName);
 
-    user?.send('Ping! If you can read this, your username is correct.');
+    user?.send('[OSU3D] Ping! If you can read this, your username is correct.');
   }
 
-  private async _onUserLevelEvent(payload: any) {
-    if (debugMode) console.log('_onUserLevelEvent()');
+  private async _onUserLevelCRUD(payload: any) {
+    if (debugMode) console.log('_onUserLevelCRUD()', payload);
     if (payload.new.level !== payload.old.level) {
       const discordName = (await this._supabaseWatcher.getDiscordName(payload.new.user_id)).data?.discord as string | null;
       if (discordName !== null)
@@ -151,8 +155,14 @@ export class Josef {
     }
   }
 
-  private async _onProfileEvent(payload: any) {
-    if (debugMode) console.log('_onProfileEvent()');
+  private async _sendUserNotification(discordName: string, message: string) {
+    const allMembers = await this._discordGuild!.members.fetch();
+    const user = allMembers.find(m => m.user.username === discordName);
+    user?.send(message);
+  }
+
+  private async _onProfilesCRUD(payload: any) {
+    if (debugMode) console.log('_onProfilesCRUD()', payload);
     if (payload.new.discord === payload.old.discord) return;
 
     if (payload.new.discord !== null)
@@ -184,38 +194,52 @@ export class Josef {
     if (roleIds !== undefined)
       await user.roles.add(roleIds);
 
-    user.send('Your osu3d.io certifications have changed! Any related Discord roles have been applied.');
+    user.send('[OSU3D] Your profile has changed! Any related Discord roles have been applied.');
   }
 
-  private async _announceMachineEvent(payload: any) {
-    if (debugMode) console.log('_announceMachineEvent()', payload);
+  private async _onMachineEventCRUD(payload: any) {
+    if (debugMode) console.log('_onMachineEventCRUD()', payload);
+
     if (payload.eventType !== 'INSERT') return;
     if (payload.new.resolved === payload.old.resolved) return;
+
+    // Do not announce STOP code
+    if (payload.new.event_type === 'STOP') {
+      if (payload.new.print_id === null) return;
+
+      const { data } = await this._supabaseWatcher.getMachineEventInfo(payload.new.id);
+      const discordName = data?.print?.created_by.discord;
+      
+      if (discordName) {
+        this._sendUserNotification(discordName, '[OSU3D] One of your prints was canceled.');
+        Timeout.clear(payload.new.print_id);
+      }
+
+      return;
+    }
     
     const { data: event } = await this._supabaseWatcher.getMachineEventInfo(payload.new.id);
     if (!event) return;
 
     const embed = new EmbedBuilder()
-        .setColor(0xFF5555)
-        .setTitle(`Fault Alert for ${event.machine?.machine_def?.model} (Tier ${event.machine?.tier.toString()})`)
-        .addFields(
-            { name: 'Printer Type', value: `${event.machine?.machine_def?.make ?? ''} ${event.machine?.machine_def?.model ?? ''}` },
-            { name: 'Issuer', value: event.created_by?.full_name ?? 'no account name' },
-            { name: 'Provided Description', value: event.description ?? '' }
-        )
-        .setTimestamp();
-        // .setURL(`${process.env.PROD_URL}/machines/${event.machine_id}`);
+      .setColor(0xFF5555)
+      .setTitle(`Fault Alert for ${event.machine?.machine_def?.model} (Tier ${event.machine?.tier.toString()})`)
+      .addFields(
+          { name: 'Printer Type', value: `${event.machine?.machine_def?.make ?? ''} ${event.machine?.machine_def?.model ?? ''}` },
+          { name: 'Issuer', value: event.created_by?.full_name ?? 'no account name' },
+          { name: 'Provided Description', value: event.description ?? '' }
+      )
+      .setTimestamp();
 
-    // this._discordDebugChannel?.send(this._getBadQuip());
     if (debugMode)
       this._discordDebugChannel?.send({ embeds: [embed] });
     else
       this._discordMachineEventChannel?.send({ embeds: [embed] });
   }
+}
 
-  private _getBadQuip() {
-    return badQuips[Math.floor(Math.random() * badQuips.length)];
-  }
+function getBadQuip() {
+  return badQuips[Math.floor(Math.random() * badQuips.length)];
 }
 
 const badQuips = [
